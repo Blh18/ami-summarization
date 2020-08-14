fbb913870d8aa48f34bc31489b9c1e41c4220a67
Blh18 <69672759+Blh18@users.noreply.github.com>
2020-08-14 12:25:29 +0200

    Delete document.py

--- model/document.py
+++
@@ -1,9 +0,0 @@
-class Document:
-    def __init__(self, doc_id, text):
-        self.id = doc_id
-        self.raw_text = text.replace('\n', ' ')
-        self.score = 0
-        self.page_rank = 1
-        self.words = dict()
-        self.sentences = dict()
-        self.graph = None
